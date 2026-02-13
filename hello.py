f29055adb4b32302e832e43afff9c7f02584da9f
seodol00 <126629503+seodol00@users.noreply.github.com>
2026-02-13 21:51:46 +0900

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+# 나의 첫 번째 Python 파일
+   
+   def greet(name):
+       return f"안녕하세요, {name}님!"
+   
+   if __name__ == "__main__":
+       print(greet("GitHub"))
+       print("첫 번째 코드를 성공적으로 작성했습니다!")
